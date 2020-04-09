125f1af807b564929cd4003883708706da8d4144
宮崎脩平 <miyazakishuhei@miyazakiosamutairanoMacBook-Pro.local>
2020-04-09 13:07:45 +0900

    大画面にも対応

--- function.php
+++ function.php
@@ -1,12 +1,76 @@
 <?php
-function spotify() {
-    require_once('vendor/autoload.php');
-    $session = new SpotifyWebAPI\Session(
-        '5076a3bdf7bc4e8f8eb0e9b15134295f',
-        '1e08120c75754a8f8de076cf79de318b'
+
+function artistSearch($artistName) {
+    global $api;
+    $artistInfo = $api->search($artistName, 'artist', array('limit' => 1));
+    if (isset($artistInfo->artists->items)) {
+        foreach ($artistInfo->artists->items as $data) {
+            $artistData = array(
+                'id' => $data->id,
+                'name' => $data->name,
+                // TODO: イメージが複数あるならランダムで表示されるようにしてもいいかも
+                'image' => $data->images[0]->url,
+                'artist_url' => $data->artist[0]->external_urls,
+            );
+        }
+        return $artistData;
+    } else {
+        // $artistInfo->artists->items がないとき
+        return false;
+        // TODO: エラーメッセージ出す？
+    }
+}
+
+function relatedArtistSearch($artistId) {
+    global $api;
+    $relatedArtist = $api->getArtistRelatedArtists($artistId)->artists;
+    $relatedArtistSelect = array();
+    $countNum = 6;
+    if (count($relatedArtist) >= $countNum) {
+        $selectionNum = $countNum;
+    } else {
+        $selectionNum = count($relatedArtist);
+    }
+    for ($i = 0; $i <= $selectionNum - 1; $i++) {
+        $relatedArtistData = array(
+            'id' => $relatedArtist[$i]->id,
+            'name' => $relatedArtist[$i]->name,
+            'images' => $relatedArtist[$i]->images[0]->url
+        );
+        // 関連アーティストを最大9つ取得
+        array_push($relatedArtistSelect, $relatedArtistData);
+    }
+    return $relatedArtistSelect;
+}
+
+function relatedArtistTopTracks($relatedArtistSelect) {
+    global $api;
+    $topTracksSelect = array();
+    foreach ($relatedArtistSelect as $data) {
+        $topTracks = $api->getArtistTopTracks($data['id'], array('country' => 'JP'))->tracks;
+        $topTracksData = array(
+            'track_id' => $topTracks[0]->id,
+            'artist_name' => $topTracks[0]->artists[0]->name,
+            'album_name' => $topTracks[0]->album->name,
+            'album_image' => $topTracks[0]->album->images[0]->url
+        );
+        if (isset($topTracks)) {
+            // 関連アーティスト各々に対してトップトラックを取得
+            array_push($topTracksSelect, $topTracksData);
+        }
+    }
+    return $topTracksSelect;
+}
+
+function relatedArtistTopAlbum($artistId) {
+    global $api;
+    $relatedArtistAlbum = $api->getArtistAlbums($artistId, array('country' => 'JP'))->items;
+    // 取得するアルバムは人気があるものとは限らない
+    $relatedArtistTopAlbum = array(
+        'artist_url' => $relatedArtistAlbum[0]->artists[0]->external_urls->spotify,
+        'artist_name' => $relatedArtistAlbum[0]->artists[0]->external_urls->name,
+        'album_image' => $relatedArtistAlbum[0]->images[0]->url,
+        'album_name' => $relatedArtistAlbum[0]->name
     );
-    $api = new SpotifyWebAPI\SpotifyWebAPI();
-    $session->requestCredentialsToken();
-    $accessToken = $session->getAccessToken();
-    $api->setAccessToken($accessToken);
+    return $relatedArtistTopAlbum;
 }
--- index.php
+++ index.php
@@ -1,81 +1,30 @@
 <?php
 require('spotify.php');
+// TODO: 関数外部化できていない
+require('function.php');
 
-    // アーティスト情報取得
-    // TODO: 空欄はエラーなのでバリデーションチェックする
-    if (isset($_POST['artistName'])) {
+// アーティスト情報取得
+// TODO: 空欄はエラーなのでバリデーションチェックする
+if (!empty($_POST['artistName'])) {
+    $artistName = $_POST['artistName'];
+    // TODO: アーティストが複数該当したら選択できるようにしたい
+    $artistData = artistSearch($artistName);
 
-        $artistName = $_POST['artistName'];
-        // TODO: アーティストが複数該当したら選択できるようにしたい
-        $artistInfo = $api->search($artistName, 'artist', array('limit' => 1));
-        if (isset($artistInfo->artists->items)) {
-            foreach ($artistInfo->artists->items as $data) {
-                $artistData = array(
-                    'id' => $data->id,
-                    'name' => $data->name,
-                    // TODO: イメージが複数あるならランダムで表示されるようにしてもいいかも
-                    'image' => $data->images[0]->url,
-                    'artist_url' => $data->artist[0]->external_urls,
-                );
-            }
-        } else {
-            // $artistInfo->artists->items がないとき
-            return false;
-            // TODO: エラーメッセージ出す？
-        }
+    // 関連アーティスト取得
+    $artistId = $artistData['id'];
+    $relatedArtistSelect = relatedArtistSearch($artistId);
 
-        // 関連アーティスト取得
-        $relatedArtist = $api->getArtistRelatedArtists($artistData['id'])->artists;
-        // var_dump($relatedArtist);
-        $relatedArtistSelect = array();
-        $countNum = 6;
-        if (count($relatedArtist) >= $countNum) {
-            $selectionNum = $countNum;
-        } else {
-            $selectionNum = count($relatedArtist);
-        }
-        for ($i = 0; $i <= $selectionNum - 1; $i++) {
-            $relatedArtistData = array(
-                'id' => $relatedArtist[$i]->id,
-                'name' => $relatedArtist[$i]->name,
-                'images' => $relatedArtist[$i]->images[0]->url
-            );
-            // 関連アーティストを最大9つ取得
-            array_push($relatedArtistSelect, $relatedArtistData);
-        }
-        // var_dump($relatedArtistSelect);
+    // 関連アーティスト表示件数
+    $countNum = 6;
+    // 関連アーティストのトップトラック取得
+    $topTracksSelect = relatedArtistTopTracks($relatedArtistSelect);
 
+    // アーティストのアルバム取得
+    $relatedArtistAlbum = relatedArtistTopAlbum($artistId);
+} else {
 
-        // 関連アーティストのトップトラック取得
-        $topTracksSelect = array();
-        foreach ($relatedArtistSelect as $data) {
-            $topTracks = $api->getArtistTopTracks($data['id'], array('country' => 'JP'))->tracks;
-            $topTracksData = array(
-                'track_id' => $topTracks[0]->id,
-                'artist_name' => $topTracks[0]->artists[0]->name,
-                'album_name' => $topTracks[0]->album->name,
-                'album_image' => $topTracks[0]->album->images[0]->url
-            );
-            if (isset($topTracks)) {
-                // 関連アーティスト各々に対してトップトラックを取得
-                array_push($topTracksSelect, $topTracksData);
-            }
-        }
-        // var_dump($topTracksData);
-        // var_dump($topTracksSelect);
-
-        // アーティストのアルバム取得
-        $relatedArtistAlbum = $api->getArtistAlbums($artistData['id'], array('country' => 'JP'))->items;
-        // 取得するアルバムは人気があるものとは限らない
-        $relatedArtistTopAlbum = array(
-            'artist_url' => $relatedArtistAlbum[0]->artists[0]->external_urls->spotify,
-            'artist_name' => $relatedArtistAlbum[0]->artists[0]->external_urls->name,
-            'album_image' => $relatedArtistAlbum[0]->images[0]->url,
-            'album_name' => $relatedArtistAlbum[0]->name
-        );
-        // var_dump($relatedArtistTopAlbum);
-    }
-        ?>
+}
+?>
 
 <html>
     <head>
@@ -93,10 +42,11 @@ require('spotify.php');
                         <h1 class="songsSearch__title">好きな音楽を見つけよう。</h1>
                         <p class="songsSearch__description">好きなアーティスト名を入力すると、あなたにピッタリの楽曲が表示されます。</p>
                         <form class="songsSearch__formCentering" action="index.php" method="post">
-                            <el-input class="hoge" type="text" name="artistName" placeholder="アーティスト名を入力" v-model="input"></el-input>
-                            <el-row class="huga">
+                            <el-input class="songsSearch__formInput" type="text" name="artistName" placeholder="アーティスト名を入力" v-model="input"></el-input>
+                            <el-row class="songsSearch__formSubmit">
                                 <el-button native-type="submit" icon="el-icon-search" circle></el-button>
                             </el-row>
+
                         </form>
                     </div>
                 </div>
@@ -106,7 +56,7 @@ require('spotify.php');
                 <h1 class="songsSearch__title">あなたにおすすめ。</h1>
                 <p class="songsSearch__description"><?php echo $artistData["name"]; ?>が好きなあなたへ。</p>
                 <div class="songsSearch__inputImage" v-for="fit in fits" :key="fit">
-                    <div class="songsSearch__inputImageBlock" style="width: 250px; display:inline-block;">
+                    <div class="songsSearch__inputImageBlock">
                         <el-image
                             style="width: 250px; height: 250px"
                             src="<?php echo $artistData["image"]; ?>"
@@ -116,9 +66,9 @@ require('spotify.php');
 
                 <div class="songsSearch__artwork" v-for="fit in fits" :key="fit">
                     <?php for ($i = 0; $i <= $countNum - 1; $i++) : ?>
-                    <div class="songsSearch__artworkBlock" style="width: 370px; height: 370px; display: inline-block;">
+                    <div class="songsSearch__artworkBlock">
                         <el-image
-                            style="width: 370px; height: 370px"
+                            style=""
                             src="<?php echo $topTracksSelect[$i]['album_image']; ?>"
                             :fit="fit"></el-image>
                             <div class="songsSearch__artworkMask">
@@ -132,6 +82,12 @@ require('spotify.php');
                                     今すぐ聞く
                                 </div>
                             </div>
+
+                        <!-- <el-image>
+                            <div slot="error" class="image-slot">
+                                <i class="el-icon-picture-outline"></i>
+                            </div>
+                        </el-image> -->
                     </div>
                     <?php endfor; ?>
                 </div>
@@ -150,8 +106,11 @@ require('spotify.php');
                 fits: ['cover'],
             },
             methods: {
-                sayHello(){
-                this.msg = "Hello!"
+                open4() {
+                    this.$notify.error({
+                    title: 'Error',
+                    message: 'This is an error message'
+                    });
                 }
             },
             mounted(){
